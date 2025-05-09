b9c86d09b5d182277a06be926e69a1554c614be6
João Vitor Belchior Estanislau <jvbelchior017@gmail.com>
2025-05-09 13:02:05 -0300

    Add files via upload

---
+++ Média Aritmética.py
@@ -0,0 +1,6 @@
+Valor1 = int(input("Digite o primeiro número: "))
+Valor2 = int(input("Digite o segundo número: "))
+valor3 = int(input("Digite o quarto número: "))
+Valor4 = int(input("Digite o quarto número: "))
+As = (Valor1 + Valor2 + valor3+ Valor4) / 4
+print("A média aritimetica simples é", As)
